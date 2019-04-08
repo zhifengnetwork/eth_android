6ee1023fa060d97ed082491658dcc1b1bb2e0af9
52PJsoso <1142569250@qq.com>
2019-04-08 15:28:01 +0800

    4.8更新

--- app/src/main/java/com/zf/eth/ui/fragment/MeFragment.kt
+++ app/src/main/java/com/zf/eth/ui/fragment/MeFragment.kt
@@ -1,8 +1,10 @@
 package com.zf.eth.ui.fragment
 
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.zf.eth.R
 import com.zf.eth.base.BaseFragment
 import com.zf.eth.ui.adapter.MeMenuAdapter
@@ -19,15 +21,29 @@ class MeFragment : BaseFragment() {
     override fun getLayoutId(): Int = R.layout.fragment_me
 
 
-    private val title = arrayListOf("支付管理", "钱包地址","C2C订单","修改密码","我的邀请","平台公告","联系客服","退出机制","下载APP")
-    private val img=arrayListOf(R.drawable.zhifu,R.drawable.qianbao,R.drawable.c2c_me,R.drawable.xiugaimima,R.drawable.yaoqing,R.drawable.gonggao,R.drawable.keuf,R.drawable.touzi,R.drawable.appicon3_16)
-
-    val adapter by lazy{MeMenuAdapter(context,title,img) }
-
+    private val title1 = arrayListOf("支付管理", "钱包地址","C2C订单","修改密码" )
+    private val img1=arrayListOf(R.drawable.zhifu,R.drawable.qianbao,R.drawable.c2c_me,R.drawable.xiugaimima,R.drawable.yaoqing,R.drawable.gonggao,R.drawable.keuf,R.drawable.touzi,R.drawable.appicon3_16)
+    private val title2=arrayListOf("我的邀请","平台公告","联系客服","退出机制","下载APP")
+    private val img2=arrayListOf(R.drawable.yaoqing,R.drawable.gonggao,R.drawable.keuf,R.drawable.touzi,R.drawable.appicon3_16)
+//    val adapter by lazy{MeMenuAdapter(context,title,img) }
+    private var divider: DividerItemDecoration? =null
     override fun initView() {
-        me_recyclerView.layoutManager=LinearLayoutManager(context)
-        me_recyclerView.adapter=adapter
-        me_recyclerView.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+        //第一个列表菜单
+        me_oneRelView.layoutManager=LinearLayoutManager(context)
+        val adapter1=MeMenuAdapter(context,title1,img1)
+        me_oneRelView.adapter=adapter1
+        //添加自定义颜色分割线
+        divider=DividerItemDecoration(context,DividerItemDecoration.VERTICAL)
+        divider!!.setDrawable(ContextCompat.getDrawable( context!!,R.drawable.shape_custom_divider)!!)
+
+        me_oneRelView.addItemDecoration(divider!!)
+
+        //第二个列表菜单
+        me_twoRelView.layoutManager=LinearLayoutManager(context)
+        val adapter2=MeMenuAdapter(context,title2,img2)
+        me_twoRelView.adapter=adapter2
+        //添加自定义颜色分割线
+        me_twoRelView.addItemDecoration(divider!!)
 
 
     }
@@ -37,4 +53,5 @@ class MeFragment : BaseFragment() {
 
     override fun initEvent() {
     }
+
 }
