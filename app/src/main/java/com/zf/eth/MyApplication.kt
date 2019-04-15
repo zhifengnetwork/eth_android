390348ba378e953308c62c81ca8f2a21da9c7c07
13620447075 <1934047406@qq.com>
2019-04-15 14:33:05 +0800

    修改密码接口

--- app/src/main/java/com/zf/eth/MyApplication.kt
+++ app/src/main/java/com/zf/eth/MyApplication.kt
@@ -2,6 +2,9 @@ package com.zf.eth
 
 import android.app.Application
 import android.content.Context
+import com.yanzhenjie.album.Album
+import com.yanzhenjie.album.AlbumConfig
+import com.zf.eth.utils.MediaLoader
 import kotlin.properties.Delegates
 
 class MyApplication : Application() {
@@ -16,6 +19,8 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+
+        Album.initialize(AlbumConfig.newBuilder((this)).setAlbumLoader(MediaLoader()).build())
 
     }
 
--- app/src/main/java/com/zf/eth/ui/activity/ChangePasswordActivity.kt
+++ app/src/main/java/com/zf/eth/ui/activity/ChangePasswordActivity.kt
@@ -2,37 +2,99 @@ package com.zf.eth.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.CountDownTimer
+import android.text.TextUtils
 import com.zf.eth.R
+import com.zf.eth.api.UriConstant
 import com.zf.eth.base.BaseActivity
+import com.zf.eth.mvp.contract.ChangePwdContract
+import com.zf.eth.mvp.presenter.ChangePwdPresenter
+import com.zf.eth.showToast
+import com.zf.eth.utils.ToastUtils
+import kotlinx.android.synthetic.main.activity_change_password.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
-class ChangePasswordActivity:BaseActivity(){
+class ChangePasswordActivity : BaseActivity(), ChangePwdContract.View {
 
-    companion object {
-        fun actionStart(context: Context?){
-            context?.startActivity(Intent(context,ChangePasswordActivity::class.java))
+    override fun setVerifyCode() {
+        showToast("验证码发送成功")
+        if (!isRun) {
+            //计时器
+            val countDownTimer = object : CountDownTimer((UriConstant.SMSLIFE * 1000).toLong(), 1000) {
+                override fun onFinish() {
+                    isRun = false
+                    getCode.text = "重新获取"
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+                    isRun = true
+                    getCode.text = "${(millisUntilFinished / 1000)}S"
+                }
+            }
+            countDownTimer.start()
         }
     }
+
+    override fun setChangePwd() {
+    }
+
+    override fun showLoading() {
+        showLoadingDialog()
+    }
+
+    override fun dismissLoading() {
+        dismissLoadingDialog()
+    }
+
+    override fun showError(msg: String, errorCode: Int) {
+        showToast(msg)
+    }
+
+    companion object {
+        fun actionStart(context: Context?) {
+            context?.startActivity(Intent(context, ChangePasswordActivity::class.java))
+        }
+    }
+
     override fun initToolBar() {
-        titleName.text="修改密码"
+        titleName.text = "修改密码"
+        backLayout.setOnClickListener { finish() }
     }
 
     override fun layoutId(): Int = R.layout.activity_change_password
+
+    private val presenter by lazy { ChangePwdPresenter() }
 
     override fun initData() {
 
     }
 
     override fun initView() {
-
+        presenter.attachView(this)
     }
 
+    private var isRun = false
+
     override fun initEvent() {
+        getCode.setOnClickListener {
+            if (TextUtils.isEmpty(phoneNum.text)) {
+                ToastUtils.showShort(this, "请输入手机号")
+            } else {
+                if (!isRun) {
+                    presenter.requestVerifyCode(phoneNum.text.toString(), "sms_changepwd")
+                }
+            }
+        }
 
     }
 
     override fun start() {
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detachView()
     }
 
 }
